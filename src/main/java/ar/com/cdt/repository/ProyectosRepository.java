cd6ae46b358876ebf124e9f01b21ae19dfb8c3a0
Leandro Saracco <leosaracco@gmail.com>
2022-10-05 15:38:58 -0300

    Delete src directory

--- src/main/java/ar/com/cdt/repository/ProyectosRepository.java
+++
@@ -1,8 +0,0 @@
-package ar.com.cdt.repository;
-
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-import ar.com.cdt.entities.Proyectos;
-
-public interface ProyectosRepository extends ElasticsearchRepository<Proyectos, String> {
-}
--- src/main/java/ar/com/cdt/repository/SocioRepository.java
+++
@@ -1,8 +0,0 @@
-package ar.com.cdt.repository;
-
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-import ar.com.cdt.entities.Socio;
-
-public interface SocioRepository extends ElasticsearchRepository<Socio, String> {
-}
